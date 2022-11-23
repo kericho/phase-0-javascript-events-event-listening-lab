ea915719f78c5f99e6888b1796992da76aefaab0
¨kericho¨ <lvisrono90@gmail.com>
2022-11-23 18:36:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-function addingEventListener() {
+const btn = document.getElementById('button');
+function clickAlert(){
+    alert('Hello theere!');
 }
+function addingEventListener() {
+    btn.addEventListener("click", clickAlert);
+}
+addingEventListener();
