c1b1bfb803b68e9f416b71981ef390bf4bd4b23b
¨kericho¨ <lvisrono90@gmail.com>
2022-12-06 05:58:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const btn = document.getElementById('button');
+/*const btn = document.getElementById('button');
 function clickAlert(){
     alert('Hello theere!');
 }
 function addingEventListener() {
     btn.addEventListener("click", clickAlert);
 }
-addingEventListener();
+addingEventListener();*/
+function addingEventListener() {
+    let input= document.getElementById("button");
+    input.addEventListener("click", addingEventListener);
+    alert("I was clicked")
+}
